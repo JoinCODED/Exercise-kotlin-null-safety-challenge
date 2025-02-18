bcc7274003224490d1364e1facb6bf634b113ca0
Yousef <yayalkandari@gmail.com>
2025-02-18 22:53:55 +0300

    fixed typo in require message

--- src/NullSafetyChallenge/null_busters.kt
+++ src/NullSafetyChallenge/null_busters.kt
@@ -7,6 +7,6 @@ val greetUser = { name: String?, address: String? ->
 
 fun calculateArea(width: Int?, height: Int?): Int {
     require(width is Int && height is Int, { "All parameters must be of type Int" })
-    require(width >=0 && height >=0, { "Parameters must of positive integers" })
+    require(width >=0 && height >=0, { "Parameters must be positive integers" })
     return width * height
 }
